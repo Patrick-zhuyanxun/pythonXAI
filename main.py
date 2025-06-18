7e414f5552c084c8f7a3e366aaf89489304eefca
Patrick-zhuyanxun <papa92616@gmail.com>
2025-06-18 13:40:28 +0800

    add main

--- main.py
+++ main.py
@@ -1,1 +1,7 @@
+import os
 
+print(
+    "Hello, World!"
+)  # This is a simple Python script that prints "Hello, World!" to the console.
+os.system("git config user.name")
+os.system("git config user.email")
